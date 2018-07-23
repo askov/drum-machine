7f2b1f21ee274f596129c87f72d567717b16582e
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-07-23 21:01:15 +0700

    added audio set

--- app/components/DrumMachine.js
+++ app/components/DrumMachine.js
@@ -32,15 +32,16 @@ export class DrumMachine extends React.Component {
       81: 'Q',
       87: 'W',
       69: 'E',
-      65: 'Q',
-      83: 'W',
-      68: 'E',
+      65: 'A',
+      83: 'S',
+      68: 'D',
       90: 'Z',
       88: 'X',
       67: 'C',
     };
     if (Object.keys(keymap).includes(keyCode)) {
       const audio = document.getElementById(keymap[keyCode]);
+      console.log('AUDIO', audio);
       if (!audio) return;
       this.playAudio(audio);
       this.updateDisplay(audio.dataset.name);
@@ -50,15 +51,15 @@ export class DrumMachine extends React.Component {
 
   render() {
     const pads = [
-      { key: 'Q', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2Fcymbal.wav?1532270251164', name: 'cymbal' },
-      { key: 'W', src: '', name: '2' },
-      { key: 'E', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2Fhi-hat.wav?1532269528250', name: 'hi-hat' },
-      { key: 'A', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2Fkick.wav?1532269631770', name: 'kick' },
-      { key: 'S', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2Fsnare-clap.wav?1532269735085', name: 'snare clap' },
-      { key: 'D', src: '', name: '6' },
-      { key: 'Z', src: '', name: '7' },
-      { key: 'X', src: '', name: '8' },
-      { key: 'C', src: '', name: '9' },
+      { key: 'Q', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20CLHH.wav?1532352722339', name: 'CLHH' },
+      { key: 'W', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20SNR%20D1.wav?1532352722730', name: 'SNR D1' },
+      { key: 'E', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20RIM%20SHT.wav?1532352722795', name: 'RIM SHT' },
+      { key: 'A', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20SNR%20D2.wav?1532352722873', name: 'SNR D2' },
+      { key: 'S', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20RIDE.wav?1532352722922', name: 'RIDE' },
+      { key: 'D', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20TOM1.wav?1532352723451', name: 'TOM1' },
+      { key: 'Z', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20TOM2.wav?1532352723760', name: 'TOM2' },
+      { key: 'X', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20COWBELL.wav?1532352724065', name: 'COWBELL' },
+      { key: 'C', src: 'https://cdn.glitch.com/0966cc53-0935-45e4-9edf-7c4fa500e219%2FRX15%20OPHH.wav?1532352724689', name: 'OPHH' },
     ];
     return (
       <div id="drum-machine">
